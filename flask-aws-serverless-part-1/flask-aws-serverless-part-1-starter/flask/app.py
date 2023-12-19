5de3f22bb67028f94979f61c88f3920f1844afdf
Yusuf Mayet <jojo786@gmail.com>
2023-12-19 11:13:07 +0200

    split starter and completed projects

--- flask-aws-serverless-part-1/flask-aws-serverless-part-1-starter/flask/app.py
+++ flask-aws-serverless-part-1/flask-aws-serverless-part-1-starter/flask/app.py
@@ -1,20 +1,8 @@
-from flask import Flask, render_template
-import os
-from boto3.dynamodb.conditions import Key
-from boto3 import resource
+from flask import Flask
 
-dynamodb = resource('dynamodb')
-posts_table = dynamodb.Table(os.environ["PostsTable"])
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
-    posts = ''
-    
-    try: 
-        response = posts_table.scan()
-        posts = response['Items']
-    except Exception as error:
-        print("dynamo scan failed:", error, flush=True) 
-              
-    return render_template('index.html', posts=posts) 
+def hello():
+    return 'Hello, World!'
